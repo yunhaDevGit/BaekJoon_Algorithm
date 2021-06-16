0841d85313b690147f1721f007db181ffbe3aced
yunhaDev <yunhaDev98@gmail.com>
2021-06-16 17:11:36 +0900

    Create CakeCutting.java

---
+++ src/main/java/Search/CakeCutting.java
@@ -0,0 +1,9 @@
+public class Test {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    
+  }
+
+}
