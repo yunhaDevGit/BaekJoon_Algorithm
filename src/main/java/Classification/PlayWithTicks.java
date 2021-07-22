e4fc7e246f520d087498a0bb901217f5d20e6d11
yunhaDevGit <yunhaDev98@gmail.com>
2021-07-22 23:56:17 +0900

    refactor: 백준 알고리즘 14696 딱지치기
    - Scanner -> BufferedReader

--- src/main/java/Classification/PlayWithTicks.java
+++ src/main/java/Classification/PlayWithTicks.java
@@ -6,8 +6,8 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class PlayWithTicks {
-    //    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static Scanner scanner = new Scanner(System.in);
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
     private static int AN, BN;
 
     static String whoseWinner(List<Integer> listA, List<Integer> listB){
@@ -23,21 +23,22 @@ public class PlayWithTicks {
         return "D";
     }
     public static void main(String[] args) throws IOException {
-        int N = scanner.nextInt();
+        int N = Integer.parseInt(br.readLine());
         List<Integer> listA = new ArrayList<>();
         List<Integer> listB = new ArrayList<>();
 
         for(int i=0;i<N;i++){
             listA.clear();
             listB.clear();
-
-            AN = scanner.nextInt();
+            st = new StringTokenizer(br.readLine());
+            AN = Integer.parseInt(st.nextToken());
             for(int j=0;j<AN;j++){
-                listA.add(scanner.nextInt());
+                listA.add(Integer.parseInt(st.nextToken()));
             }
-            BN = scanner.nextInt();
+            st = new StringTokenizer(br.readLine());
+            BN = Integer.parseInt(st.nextToken());
             for(int j=0;j<BN;j++){
-                listB.add(scanner.nextInt());
+                listB.add(Integer.parseInt(st.nextToken()));
             }
             System.out.println(whoseWinner(listA, listB));
         }
